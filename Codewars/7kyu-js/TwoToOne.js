b1b02d6bc076267e5adca8be2d2318c70a52a631
QuackersNCheese <Quacktastrophe@protonmail.com>
2025-02-18 09:48:11 -0700

    array to set to array conversion

---
+++ Codewars/7kyu-js/TwoToOne.js
@@ -0,0 +1,24 @@
+/* Codewars
+Take 2 strings s1 and s2 including only letters from a to z. Return a new 
+sorted string (alphabetical ascending), the longest possible, containing 
+distinct letters - each taken only once - coming from s1 or s2.
+
+Examples:
+a = "xyaabbbccccdefww"
+b = "xxxxyyyyabklmopq"
+longest(a, b) -> "abcdefklmopqwxy"
+
+a = "abcdefghijklmnopqrstuvwxyz"
+longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+Fundamentals*/
+/*const smash = s => Array.from(new Set([...s])).sort().join('');
+function longest(s1, s2) {
+  return smash(s1 + s2);
+}*/
+
+const longest = (s1, s2) => Array.from(new Set([...(s1 + s2)])).sort().join('');
+
+// Testing
+console.log(longest("aretheyhere", "yestheyarehere") == "aehrsty")
+console.log(longest("loopingisfunbutdangerous", "lessdangerousthancoding") == "abcdefghilnoprstu")
+console.log(longest("inmanylanguages", "theresapairoffunctions") == "acefghilmnoprstuy")
