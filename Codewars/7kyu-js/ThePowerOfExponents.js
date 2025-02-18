76c03df5799fe9de5679319ee54e960dbd56e547
QuackersNCheese <Quacktastrophe@protonmail.com>
2025-02-18 04:01:25 -0700

    deep explore recursion and edge cases

--- Codewars/7kyu-js/ThePowerOfExponents.js
+++ Codewars/7kyu-js/ThePowerOfExponents.js
@@ -14,13 +14,16 @@ Examples:
 power(2, 3)   // 8
 power(4, -2)  // 0.0625
 MathematicsAlgorithms*/
-
+// #### recursive ####
 /*function power(base, exp) {
   if(exp == 0) {return 1;}
   if(exp > 0) {return base * power(base, exp - 1);}
   if(exp < 0) {return power(base, exp + 1) / base;}
 }*/
-function power(base, exp) {
+//const power = (b, e) => e != 0 ? 1 : e < 0 ? 1/b * power(b, ++e) : b * power(b, --e);  
+//const power = (b, e) => e < 0 ? 1/b * power(b, ++e) : e > 0 ? b * power(b, --e) : 1;
+// #### C-style ####
+/*function power(base, exp) {
   let b, e, result = 1;
 
   if(exp < 0) { 
@@ -34,12 +37,20 @@ function power(base, exp) {
     result *= b;
   
   return result;
-}
-/*const power = (b, e) => [...Array(e < 0 ? -e : e)].map((x) => x = (e < 0 ? 1/b : b)).reduce((s, c) => s * c, 1);*/
+}*/
+//const power = (b, e) => [...Array(e < 0 ? -e : e)].map((x) => x = (e < 0 ? 1/b : b)).reduce((s, c) => s * c, 1);
 const power = (b, e) => [...Array(e < 0 ? -e : e)].reduce((s) => s * (e < 0 ? 1/b : b), 1);
-/*// from macnick
-const power = (b, e) => e > 1 ? b * power(b, --e): e < 1 ? 1 / b * power(b, ++e) : b;*/
+// #### from macnick ####
+//const power = (b, e) => e > 1 ? b * power(b, --e): e < 1 ? 1/b * power(b, ++e) : b;
 
 // Testing
-console.log(power(2, 3), 8);
-console.log(power(4, -2), 0.0625, "power(x,negative) should be equal 1/power(x,positive)");
+console.log(power(2, 3) == 8 ? "Pass" : "Fail: 2^3");
+console.log(power(4, -2) == 0.0625 ? "Pass" : "Fail: 4^-2");
+console.log(power(-2, 3) == -8 ? "Pass" : "Fail: (-2)^3");
+console.log(power(-2,-3) == -0.125 ? "Pass" : "Fail: (-2)^-3");
+console.log(power(-2, -4,) == 1/16 ? "Pass" : "Fail: (-2)^-4");
+console.log(power(2, 0) == 1 ? "Pass" : "Fail: 2^0");
+console.log(power(2, 1) == 2 ? "Pass" : "Fail: 2^1");
+console.log(power(0, 1) === 0 ? "Pass" : "Fail: 0^1");
+console.log(power(0, -3), 'Infinity'); // unfair  
+console.log(power(0, 0), 'NaN');  // unfair
